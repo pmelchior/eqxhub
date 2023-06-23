9dd8a86f4db406df4d03de05f0441af5569bb491
Peter Melchior <peter.m.melchior@gmail.com>
2023-06-23 00:34:01 -0400

    Torch.hub clone with minimal edits to work with equinox tree serialization

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="eqxhub",
+      description="Torch.hub-like clone for equinox models",
+      long_description="Load code and pretrained models from entrypoints",
+      version="0.1",
+      license="MIT",
+      author="Peter Melchior",
+      author_email="peter.m.melchior@gmail.com",
+      py_modules=["eqxhub"],
+      url="https://github.com/pmelchior/eqxhub",
+      requires=[]
+)
+
---
+++ test.py
@@ -0,0 +1,4 @@
+import equinox as eqx
+import eqxhub as hub
+
+print(hub.load("pmelchior/eqxhubtest", "test"))
